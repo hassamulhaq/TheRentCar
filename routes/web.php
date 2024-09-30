bf78f478b3cf90e9406ac8c652ba688433e0fbe5
hassamulhaq <developer.hassam@gmail.com>
2024-09-30 19:16:40 +0500

    Fix route issue

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,24 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-
 Route::group([
+    'name' => 'vehicles.',
     'as' => 'vehicles.',
 ], function () {
     Route::get('/vehicles', \App\Livewire\VehicleListPage::class)
         ->name('list');
 
-    Route::get('/{vehicle:slug}', \App\Livewire\VehicleView::class)
+    Route::get('vehicles/{vehicle:slug}', \App\Livewire\VehicleView::class)
         ->name('view');
 
-    Route::get('reservation/{vehicle:slug}', function () {
+    Route::get('vehicles/{vehicle:slug}/reservation', function () {
         // Implementation will be added soon
-        dd('will be implemented soon');
     })->name('reservation');
 });
+
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
