02e39f28e8eecebeb253e42e0f55f1d318f6ea03
hassamulhaq <developer.hassam@gmail.com>
2024-12-01 15:21:43 +0500

    Fix title if seo metadata found

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
         @if(isset($seo))
             {!! $seo !!}
+        @else
+            <title>{{ config('app.name', 'Laravel') }}</title>
         @endif
 
         <!-- Fonts -->
